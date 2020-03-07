aa6f47b76ff5dc473612d446e43ee9e3302b5466
IsisPMR <piorctt@gmail.com>
2020-03-07 11:16:37 -0600

    Updates

---
+++ precache-manifest.ae0b04b355b5ed3be53f29b281cdb4b6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426522cbd75f0e265992132a8441799f",
+    "url": "/Chameleon/index.html"
+  },
+  {
+    "revision": "71b4de4c8b7f27a34583",
+    "url": "/Chameleon/static/css/main.e269faff.chunk.css"
+  },
+  {
+    "revision": "3e942eb5021cc77d3416",
+    "url": "/Chameleon/static/js/2.3c66ac53.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Chameleon/static/js/2.3c66ac53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b4de4c8b7f27a34583",
+    "url": "/Chameleon/static/js/main.3a3b1bd3.chunk.js"
+  },
+  {
+    "revision": "bd0d75c0aaaaf2e23cd5",
+    "url": "/Chameleon/static/js/runtime-main.0c7229b2.js"
+  },
+  {
+    "revision": "84c84cba938aa2ad1e0a8997402bf8ee",
+    "url": "/Chameleon/static/media/Excersice.84c84cba.PNG"
+  },
+  {
+    "revision": "2be0be92abffa113790a3255c87060a1",
+    "url": "/Chameleon/static/media/Hacks.2be0be92.PNG"
+  },
+  {
+    "revision": "8d55cb94760031a2962ffc988c3aed30",
+    "url": "/Chameleon/static/media/Healthy.8d55cb94.jpg"
+  },
+  {
+    "revision": "15dae0ea9b0d6a735d046bfcf4d69579",
+    "url": "/Chameleon/static/media/LifeStyle.15dae0ea.PNG"
+  },
+  {
+    "revision": "0ac0cfd3b4cd4d6e9957b70ea228ac28",
+    "url": "/Chameleon/static/media/LoveAndRelationships.0ac0cfd3.PNG"
+  },
+  {
+    "revision": "7ecf0346e31de9032788dd1c0ee55d80",
+    "url": "/Chameleon/static/media/Music.7ecf0346.PNG"
+  },
+  {
+    "revision": "955f8250c74738482f9fc0f28544cf5d",
+    "url": "/Chameleon/static/media/Pets.955f8250.PNG"
+  },
+  {
+    "revision": "43859fab044373d8150999cb8d216c12",
+    "url": "/Chameleon/static/media/SelfDefense.43859fab.PNG"
+  },
+  {
+    "revision": "8adc6098c40ee18c5cb5e3648892bd4e",
+    "url": "/Chameleon/static/media/chameleon.8adc6098.jpg"
+  }
+]);
