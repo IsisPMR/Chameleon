b3c70e4b63f47f2c4ed45ba4b5f3ced44d41daf8
IsisPMR <piorctt@gmail.com>
2020-03-12 22:24:15 -0600

    Routes are fixed now / upload tutorial page with all the fields

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
-import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import "./Details.css";
+import Button from '@material-ui/core/Button';
 
 class Details extends Component {
   render() {
     return (
-
       <div className="containerForm">
-        <h1>Upload a new tutorial</h1>
-        <TextField required id="standard-required" label="Tutorial Name" defaultValue="Name" /><br/><br/>
-          <Select className="fieldwith">
+        <h2>Upload a new tutorial</h2>
+        <TextField
+          required
+          id="standard-required"
+          label="Tutorial Name"
+          defaultValue="Name"
+        />
+        <br />
+        <br />
+        <Select className="fieldwith">
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
@@ -23,7 +30,14 @@ class Details extends Component {
           <MenuItem value={5}>Healthy</MenuItem>
           <MenuItem value={6}>Music</MenuItem>
           <MenuItem value={7}>Excersice</MenuItem>
-        </Select><br/><br/>
+        </Select>
+        <br />
+        <br />
+        <p>Upload file</p>
+        <input ref="upload" type="file" />
+        <br />
+        <br />
+        <Button>Sumbit</Button>
       </div>
     );
   }
