06cfce1febd60281339aabdedd37c78054bd8229
IsisPMR <piorctt@gmail.com>
2020-03-17 08:56:32 -0600

    Updates

---
+++ precache-manifest.a3eed15a1d22f7620c9a9690aa5abd2e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72b225b62834d31d8cb1ee6a0132edd",
+    "url": "/Chameleon/index.html"
+  },
+  {
+    "revision": "7954612e03237ab39883",
+    "url": "/Chameleon/static/css/main.70104a50.chunk.css"
+  },
+  {
+    "revision": "898eeff4835cafb7fc09",
+    "url": "/Chameleon/static/js/2.a2421a5b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Chameleon/static/js/2.a2421a5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7954612e03237ab39883",
+    "url": "/Chameleon/static/js/main.0afcf032.chunk.js"
+  },
+  {
+    "revision": "bd0d75c0aaaaf2e23cd5",
+    "url": "/Chameleon/static/js/runtime-main.0c7229b2.js"
+  },
+  {
+    "revision": "28dc5cbf6ff6290df02ef88045225fb9",
+    "url": "/Chameleon/static/media/Aerobic1.28dc5cbf.PNG"
+  },
+  {
+    "revision": "5c9b5ebe879375c5c9b0589d9bb6ae87",
+    "url": "/Chameleon/static/media/ChameleonLogIn - copia.5c9b5ebe.ai"
+  },
+  {
+    "revision": "81775261e777a474dae4ab036a43e287",
+    "url": "/Chameleon/static/media/ChameleonLogIn - copia.81775261.jpg"
+  },
+  {
+    "revision": "81775261e777a474dae4ab036a43e287",
+    "url": "/Chameleon/static/media/ChameleonLogIn.81775261.jpg"
+  },
+  {
+    "revision": "84c84cba938aa2ad1e0a8997402bf8ee",
+    "url": "/Chameleon/static/media/Excersice.84c84cba.PNG"
+  },
+  {
+    "revision": "8d55cb94760031a2962ffc988c3aed30",
+    "url": "/Chameleon/static/media/Health.8d55cb94.jpg"
+  },
+  {
+    "revision": "a7c7abf31833e1d4e203dc03806f57f9",
+    "url": "/Chameleon/static/media/HighKick.a7c7abf3.PNG"
+  },
+  {
+    "revision": "2be0be92abffa113790a3255c87060a1",
+    "url": "/Chameleon/static/media/LifeHacks.2be0be92.PNG"
+  },
+  {
+    "revision": "15dae0ea9b0d6a735d046bfcf4d69579",
+    "url": "/Chameleon/static/media/LifeStyle.15dae0ea.PNG"
+  },
+  {
+    "revision": "3b70322153fd569c8e0cfd76cc49ca20",
+    "url": "/Chameleon/static/media/Love.3b703221.PNG"
+  },
+  {
+    "revision": "0ac0cfd3b4cd4d6e9957b70ea228ac28",
+    "url": "/Chameleon/static/media/LoveAndRelationships.0ac0cfd3.PNG"
+  },
+  {
+    "revision": "7ecf0346e31de9032788dd1c0ee55d80",
+    "url": "/Chameleon/static/media/Music.7ecf0346.PNG"
+  },
+  {
+    "revision": "955f8250c74738482f9fc0f28544cf5d",
+    "url": "/Chameleon/static/media/Pets.955f8250.PNG"
+  },
+  {
+    "revision": "b8160b2d01b7cf5fb63c3d6426be20c9",
+    "url": "/Chameleon/static/media/Recurso 4-100.b8160b2d.jpg"
+  },
+  {
+    "revision": "43859fab044373d8150999cb8d216c12",
+    "url": "/Chameleon/static/media/SelfDefense.43859fab.PNG"
+  },
+  {
+    "revision": "905ca59d772fdb749b0b5659c2eae2cb",
+    "url": "/Chameleon/static/media/Travels.905ca59d.PNG"
+  },
+  {
+    "revision": "e9e77779958fae572f0012fe17ad7da0",
+    "url": "/Chameleon/static/media/User2.e9e77779.PNG"
+  },
+  {
+    "revision": "89b9e99e7726e3b4ac186611fc1eb849",
+    "url": "/Chameleon/static/media/chameleon.89b9e99e.png"
+  },
+  {
+    "revision": "c80293ec137c6c52b7dc5fb985128fd9",
+    "url": "/Chameleon/static/media/chameleon.c80293ec.jpg"
+  },
+  {
+    "revision": "cc8b017312d4243de101f4d00589893d",
+    "url": "/Chameleon/static/media/delete.cc8b0173.png"
+  },
+  {
+    "revision": "dc789b5d31c7c9b790031497baa3ec55",
+    "url": "/Chameleon/static/media/video-camera.dc789b5d.png"
+  }
+]);
