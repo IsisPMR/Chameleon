991d8c6fb43af5e2e0b96bba0e9db05a99d493a4
IsisPMR <piorctt@gmail.com>
2020-03-21 16:10:57 -0600

    Before axios

--- src/components/BlogCategory/BlogCategory.js
+++ src/components/BlogCategory/BlogCategory.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import data from "../../data.json";
 import Grid from "@material-ui/core/Grid";
 import ReactPlayer from "react-player";
+import redDelete from "../../img/icons/delete.png";
 import "./BlogCategory.css";
+
 
 export default class BlogCategory extends Component {
 
@@ -33,6 +35,7 @@ export default class BlogCategory extends Component {
 
     return (
       <div className="allVideosContainer">
+        <div><h3>Pulsa la equis para eliminar un video</h3></div>
         <div className="onTop">
         <ReactPlayer
           url={currentVideo ? currentVideo : item.length > 0 && item[0].mp4}
@@ -45,11 +48,14 @@ export default class BlogCategory extends Component {
             item.map((l, index) => {
               return (
                 <Grid  key={index} xs className="detailItems">
+                  <img src={ redDelete } alt={"supr"} className="redX"/>
                   <img onClick={() => this.changeImage(l.mp4)} style={{ width: 210, height: 118 }} alt={l.title} src={l.src} />
-                  <div>{l.title}</div>
+                  <div className="textStyle">
+                  <div className="mainTitle">{l.title}</div>
                   <div>{l.channel}</div>
                   <div>{l.views}</div>
                   <div>{l.createdAt}</div>
+                  </div>
                 </Grid >
               )
             })
